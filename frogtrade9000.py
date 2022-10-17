a74e0294b2e6f9a505bc472a9c75fb45940d11ae
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-10-17 18:30:17 +0200

    formating: replace '' with ""

--- frogtrade9000.py
+++ frogtrade9000.py
@@ -946,7 +946,7 @@ def main():
                             name, client = setup_client(name=botname, url=url, port=port, username=suser, password=spass)
                         client_dict[name] = client
                     except Exception as e:
-                        raise RuntimeError('Cannot create freqtrade client') from e
+                        raise RuntimeError("Cannot create freqtrade client") from e
                 else:
                     raise Exception("Cannot parse server option. Please use [name]user:pass@servername:port")
     elif config is not None:
